a82c619799f617a3ffb7558c795819f9a857d3cb
Hong-En09 <tanhe-wm19@student.tarc.edu.my>
2022-09-29 01:13:59 +0800

    Modified FarmerActivity

--- app/src/main/java/com/example/mobileassignment/ui/Homepage/FarmerActivity.kt
+++ app/src/main/java/com/example/mobileassignment/ui/Homepage/FarmerActivity.kt
@@ -7,18 +7,17 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileassignment.R
 import com.example.mobileassignment.databinding.ActivityFarmerBinding
 import com.example.mobileassignment.ui.UserManagement.UserActivity
+import com.google.android.material.navigation.NavigationView
 
 class FarmerActivity : AppCompatActivity() {
 
@@ -33,10 +32,6 @@ class FarmerActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarFarmer.toolbar)
 
-        binding.appBarFarmer.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_farmer)
@@ -44,7 +39,7 @@ class FarmerActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.farmerHomepageFragment
+                R.id.farmerHomepageFragment, R.id.nav_productList
             ), drawerLayout
         )
 
