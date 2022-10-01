8f723ed91273811ba65e2536cb879bba23a879ad
Hong-En09 <tanhe-wm19@student.tarc.edu.my>
2022-10-01 17:10:52 +0800

    ContractDetailActivity

---
+++ app/src/main/java/com/example/mobileassignment/ui/FarmerFolder/ContractDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mobileassignment.ui.FarmerFolder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileassignment.R
+
+class ContractDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contract_detail)
+
+
+    }
+}
