ff67ee79a950ec25bd39191be2d881ee729e63bd
Rave <lowyz-wm19@student.tarc.edu.my>
2022-10-03 01:23:18 +0800

    Update

--- app/src/main/java/com/example/mobileassignment/Entity/ProductList.kt
+++ app/src/main/java/com/example/mobileassignment/Entity/ProductList.kt
@@ -1,14 +1,17 @@
 package com.example.mobileassignment.Entity
 
+import android.graphics.Bitmap
 import java.io.Serializable
 
 class ProductList : Serializable{
 
     var name:String? = null
-    var image:Int? = null;
+    var image:Bitmap? = null;
 
-    constructor(name:String, image:Int){
+    constructor(name:String, image:Bitmap){
         this.name = name;
         this.image = image;
     }
+
+    constructor()
 }
