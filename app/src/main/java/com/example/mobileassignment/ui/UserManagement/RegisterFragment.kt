4d987ef9659167d7206c2320d7072cd5f91ab987
Rave <lowyz-wm19@student.tarc.edu.my>
2022-10-03 05:31:08 +0800

    Update

--- app/src/main/java/com/example/mobileassignment/ui/UserManagement/RegisterFragment.kt
+++ app/src/main/java/com/example/mobileassignment/ui/UserManagement/RegisterFragment.kt
@@ -1,7 +1,9 @@
 package com.example.mobileassignment.ui.UserManagement
 
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +18,7 @@ import com.example.mobileassignment.R
 import com.example.mobileassignment.databinding.FragmentRegisterBinding
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 
@@ -57,51 +60,140 @@ class RegisterFragment: Fragment() {
             val phone = binding.registerPhoneNumber.text.toString()
             val address = binding.address.text.toString()
             val photoURL = ""
+            var usernameMatch = ""
+
+
+
+
+
 
             var errorMsg: Unit? = null
+            if(username.isEmpty()){
+                binding.textValidationUsername.isVisible = true
+                errorMsg = binding.textValidationUsername.setText("Required")
+            }else if(username.isNotEmpty()){
+                val databaseRef = FirebaseDatabase.getInstance().reference.child("user")
+                    .orderByChild("username").equalTo(username)
+                databaseRef.addValueEventListener(object: ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        for(ds in snapshot.children){
+                            usernameMatch = ds.child("username").getValue(String::class.java).toString()
+                        }
+                        if(username.isEmpty()){
+                            binding.textValidationUsername.isVisible = true
+                            errorMsg = binding.textValidationUsername.setText("Required")
+                        }else if(username == usernameMatch){
+                            binding.textValidationUsername.isVisible = true
+                            errorMsg = binding.textValidationUsername.setText("Username already exist.")
+                        }else{
+                            binding.textValidationUsername.isVisible = false
+                        }
+
+                        if(email.isEmpty()){
+                            binding.textValidationEmail.isVisible = true
+                            errorMsg = binding.textValidationEmail.setText("Required")
+
+                        }else if(!email.matches("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})".toRegex())){
+                            binding.textValidationEmail.isVisible = true
+                            errorMsg = binding.textValidationEmail.setText("Wrong email format")
+
+                        }else{
+                            binding.textValidationEmail.isVisible = false
+                        }
+
+                        if(phone.isEmpty()){
+                            binding.textValidationPhoneNumber.isVisible = true
+                            errorMsg =  binding.textValidationPhoneNumber.setText("Required")
+                        }else{
+                            binding.textValidationPhoneNumber.isVisible = false
+                        }
+
+                        if(address.isEmpty()){
+                            binding.textValidationAddress.isVisible = true
+                            errorMsg = binding.textValidationAddress.setText("Required")
+
+                        }else{
+                            binding.textValidationAddress.isVisible = false
+                        }
+
+                        if(password.isEmpty()){
+                            binding.textValidationPassword.isVisible = true
+                            errorMsg = binding.textValidationPassword.setText("Required")
+                        }else if(password.length < 8){
+                            binding.textValidationPassword.isVisible = true
+                            errorMsg = binding.textValidationPassword.setText("Minimum 8 Character Password")
+                        }
+                        else if(!password.matches(".*[@#\\\$%^&+=].*".toRegex())){
+                            binding.textValidationPassword.isVisible = true
+                            errorMsg = binding.textValidationPassword.setText("Must Contains 1 Special Character")
+                        }else{
+                            binding.textValidationPassword.isVisible = false
+                        }
+
+                        if(confirmPass.isEmpty()){
+                            binding.textValidationConfirmPassword.isVisible = true
+                            errorMsg = binding.textValidationConfirmPassword.setText("Required")
+
+                        }else if(confirmPass != password){
+                            binding.textValidationConfirmPassword.isVisible = true
+                            errorMsg = binding.textValidationConfirmPassword.setText("Confirm Password must match with Password")
+                        }else{
+                            binding.textValidationConfirmPassword.isVisible = false
+                        }
+
+                        if(binding.roleSpinner.selectedItem.toString().trim() == "Please Select Role"){
+                            binding.textValidationRoleSpinner.isVisible = true
+                            errorMsg = binding.textValidationRoleSpinner.setText("Required")
+
+                        }else if(binding.roleSpinner.selectedItem.toString().trim() == "Farmer" || binding.roleSpinner.selectedItem.toString().trim() == "Client"){
+                            binding.textValidationRoleSpinner.isVisible = false
+                        }
+
+                        if(errorMsg == null) {
+                            val newUser = User(username, password, role, email,phone,address,photoURL)
+
+                            val database: DatabaseReference = Firebase.database.getReference("user")
+                            database.child(newUser.username).setValue(newUser)
+                            Toast.makeText(context, "Profile Saved", Toast.LENGTH_SHORT).show()
+                            findNavController().navigate(R.id.nav_login)
+
+                        }
+
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            }else if(username == usernameMatch){
+                binding.textValidationUsername.isVisible = true
+                binding.textValidationUsername.setText("Username already exist.")
+        }else{
+            binding.textValidationUsername.isVisible = false
+        }
 
             if(email.isEmpty()){
-               errorMsg = binding.textValidationEmail.setText("Required")
+                binding.textValidationEmail.isVisible = true
+                errorMsg = binding.textValidationEmail.setText("Required")
 
             }else if(!email.matches("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})".toRegex())){
-               errorMsg = binding.textValidationEmail.setText("Wrong email format")
+                binding.textValidationEmail.isVisible = true
+                errorMsg = binding.textValidationEmail.setText("Wrong email format")
 
             }else{
                 binding.textValidationEmail.isVisible = false
             }
 
-            if(username.isEmpty()){
-                errorMsg = binding.textValidationUsername.setText("Required")
-            }else{
-                binding.textValidationUsername.isVisible = false
-            }
-
-
-            val databaseRef = FirebaseDatabase.getInstance().reference.child("user")
-                .orderByChild("username").equalTo(username)
-
-
-            databaseRef.addValueEventListener(object: ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-
-                    if(username.equals(databaseRef)){
-                        errorMsg = binding.textValidationUsername.setText("Username Exist")
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-
             if(phone.isEmpty()){
+                binding.textValidationPhoneNumber.isVisible = true
                 errorMsg =  binding.textValidationPhoneNumber.setText("Required")
             }else{
                 binding.textValidationPhoneNumber.isVisible = false
             }
 
             if(address.isEmpty()){
+                binding.textValidationAddress.isVisible = true
                 errorMsg = binding.textValidationAddress.setText("Required")
 
             }else{
@@ -109,46 +201,37 @@ class RegisterFragment: Fragment() {
             }
 
             if(password.isEmpty()){
+                binding.textValidationPassword.isVisible = true
                 errorMsg = binding.textValidationPassword.setText("Required")
             }else if(password.length < 8){
+                binding.textValidationPassword.isVisible = true
                 errorMsg = binding.textValidationPassword.setText("Minimum 8 Character Password")
             }
             else if(!password.matches(".*[@#\\\$%^&+=].*".toRegex())){
+                binding.textValidationPassword.isVisible = true
                 errorMsg = binding.textValidationPassword.setText("Must Contains 1 Special Character")
             }else{
                 binding.textValidationPassword.isVisible = false
             }
 
             if(confirmPass.isEmpty()){
+                binding.textValidationConfirmPassword.isVisible = true
                 errorMsg = binding.textValidationConfirmPassword.setText("Required")
 
             }else if(confirmPass != password){
+                binding.textValidationConfirmPassword.isVisible = true
                 errorMsg = binding.textValidationConfirmPassword.setText("Confirm Password must match with Password")
             }else{
                 binding.textValidationConfirmPassword.isVisible = false
             }
 
             if(binding.roleSpinner.selectedItem.toString().trim() == "Please Select Role"){
+                binding.textValidationRoleSpinner.isVisible = true
                 errorMsg = binding.textValidationRoleSpinner.setText("Required")
 
             }else if(binding.roleSpinner.selectedItem.toString().trim() == "Farmer" || binding.roleSpinner.selectedItem.toString().trim() == "Client"){
                 binding.textValidationRoleSpinner.isVisible = false
             }
-
-            if(errorMsg == null) {
-                val newUser = User(username, password, role, email,phone,address,photoURL)
-
-                val database: DatabaseReference = Firebase.database.getReference("user")
-                database.child(newUser.username).setValue(newUser)
-                Toast.makeText(context, "Profile Saved", Toast.LENGTH_SHORT).show()
-                findNavController().navigate(R.id.nav_login)
-
-            }
-
-                //Return back to the First Fragment
-                //val navController = activity?.findNavController(R.id.nav_host_fragment_content_main)
-                //navController?.navigateUp()
-                //findNavController().navigateUp()
         }
     }
 
